164b3b4da65e9f994ecaa9f27f62b35529bcdb40
Steven Smith <github@icodeforyou.com>
2020-11-07 13:59:37 -0500

    basic template

--- password_package_example/test/widget_test.dart
+++ password_package_example/test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:password_package_example/main.dart';
 
 void main() {
--- test/password_package_test.dart
+++ test/password_package_test.dart
@@ -1,13 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:password_package/password_package.dart';
+import 'package:password_package/src/password_manager.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    final state = PasswordState.hide;
+    expect(state, equals(PasswordState.hide));
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
